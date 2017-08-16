4960b443d11510d8cae710047c0586fa03c836cb
Brian Barnes <brian@decisivebison.com>
2017-08-16 00:12:18 -0400

    Schedule formatting

--- marathon2/script.js
+++ marathon2/script.js
@@ -5,23 +5,29 @@ function generateTable(){
 		var game = games[i]
 		$row = $("<tr>", {class: "sched_row", id:game["abbr"] + "_row"});
 
-		$row.append($("<td>").append(game["name"]));
+		$row.append($("<td>", {class: "sched_title"}).append(game["name"]));
 		
 		if (game["start"]) {
 			var time = new Date(game["start"]);
 		} else {
 			var prevTime = new Date(games[i-1]["start"])
-			var time = new Date(prevTime.getTime() + (60000 * games[i-1]["estimate"]) + (60000 * 15))
+			var time = new Date(prevTime.getTime() + (60000 * games[i-1]["estimate"]) + (60000 * 30))
 			game["start"] = time.toISOString()
 		}
-		$row.append($("<td>").append(time.toLocaleString()));
+		$row.append($("<td>").append(time.toLocaleString([], {
+																weekday: "short",
+																month: "numeric",
+																day: "numeric",
+																hour: "numeric",
+																minute: "numeric"
+															 })));
 
 		$row.append($("<td>").append(game["complete"] ? minutesToHM(game["complete"]) : minutesToHM(game["estimate"]) + " (est.)"));
 
 		$row.append($("<td>").append(game["player"]));
 
 		if ("hidden" in game) {
-			$row.css("display", "hidden")
+			$row.css("display", "none")
 		}
 
 		games[i] = game;
