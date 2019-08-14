32c0b5abcaeb34e8cd54c12be82f0f1b2400e35f
Brian Barnes <brian@decisivebison.com>
2019-08-14 19:45:37 -0400

    Preparing to put the site live

--- marathon4/script/db.php
+++ marathon4/script/db.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 include("../../../../dbinfo.php");
-$mysqli = new mysqli("localhost", $user, $password, "marathon");
+$mysqli = new mysqli("localhost", $user, $password, "marathon4");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
